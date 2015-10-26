439c750bfd3fb19ac2e157ef249cff7f4e6857f7
smad2005 <smad2005@gmail.com>
2015-10-26 20:02:28 +0200

    Implemented Floating Combat Display This closes smad2005/PoeHud#18

--- src/Hud/Health/Healthbar.cs
+++ src/Hud/Health/Healthbar.cs
@@ -1,12 +1,20 @@
+using System.Collections.Generic;
+using System.Diagnostics;
 using PoeHUD.Models;
 using PoeHUD.Models.Enums;
 using PoeHUD.Poe.Components;
+using SharpDX.Direct3D9;
 
 namespace PoeHUD.Hud.Health
 {
     public class HealthBar
     {
         private readonly bool isHostile;
+        private int lastHp;
+        private Stopwatch dpsStopwatch = Stopwatch.StartNew();
+        private const int DPS_CHECK_TIME = 1000;
+        private const int DPS_FAST_CHECK_TIME = 200;
+        private const int DPS_POP_TIME = 2000;
 
         public HealthBar(EntityWrapper entity, HealthBarSettings settings)
         {
@@ -49,19 +57,61 @@ namespace PoeHUD.Hud.Health
                     Settings = settings.Minions;
                 }
             }
+            Life = Entity.GetComponent<Life>();
+            lastHp = GetFullHp();
         }
 
-        public EntityWrapper Entity { get; private set; }
+        public Life Life { get; }
+
+        public EntityWrapper Entity { get; }
 
         public bool IsValid { get; private set; }
 
-        public UnitSettings Settings { get; private set; }
+        public UnitSettings Settings { get; }
 
         public CreatureType Type { get; private set; }
 
         public bool IsShow(bool showEnemy)
         {
             return !isHostile ? Settings.Enable.Value : Settings.Enable && showEnemy && isHostile;
+        }
+
+        public LinkedList<int> DpsQueue { get; } = new LinkedList<int>();
+
+        public void DpsRefresh()
+        {
+            var chechTime = (DpsQueue.Count > 0 ? DPS_CHECK_TIME : DPS_FAST_CHECK_TIME);
+            if (dpsStopwatch.ElapsedMilliseconds >= chechTime)
+            {
+                var hp = GetFullHp();
+                if (hp > -1000000 && hp < 10000000 && lastHp != hp)
+                {
+                    DpsQueue.AddFirst(-(lastHp - hp));
+                    if (DpsQueue.Count > Settings.FloatingCombatStackSize)
+                    {
+                        DpsQueue.RemoveLast();
+                        dpsStopwatch.Restart();
+                    }
+                    lastHp = hp;
+                }
+            }
+        }
+
+        public void DpsDequeue()
+        {
+            if (dpsStopwatch.ElapsedMilliseconds >= DPS_POP_TIME)
+            {
+                if (DpsQueue.Count > 0)
+                {
+                    DpsQueue.RemoveLast();
+                }
+                dpsStopwatch.Restart();
+            }
+        }
+
+        private int GetFullHp()
+        {
+            return Life.CurHP + Life.CurES;
         }
     }
 }
